8f68cbdd09fd018768969835e47e2144b998fa1c
Uwe Michael Scheffer <uwe.scheffer@scheffer-software.de>
2018-11-04 18:11:54 +0100

    Add JS for preloading all the main images up front

--- js/gallery.js
+++ js/gallery.js
@@ -24,6 +24,23 @@ function activateGallery() {
         // The class name marking the current thumbnail.
         let theClass = "current";
 
+        // When clicking on a new thumbnail image on the live site, you might
+        // notice a slight delay before the main image appears in the center.
+        // This is because, unlike the thumbnails, the large versions haven’t
+        // been downloaded yet.
+
+        // It’s a common practice to prevent this small but annoying delay by
+        // preloading the images in the background, thereby putting them into
+        // the browser cache - a task we can accomplish with JavaScript.
+
+        // The trick is to create a new Image object and assign it the src of
+        // the main image corresponding to each thumbnail forcing the browser
+        // to download all the large images - before the page is even loaded.
+
+        // Preload the large images.
+        let largeVersion = new Image();
+        largeVersion.src = largeImageSrc;
+
         thumbnail.addEventListener("click", function() {
             // Set main image's src, alt and title properties, all-in-one-go.
             Object.assign(largeImage, {
