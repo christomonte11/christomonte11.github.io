8b42279082a8031d6b23144b9a24cb605ede7e6f
Uwe Michael Scheffer <uwe.scheffer@scheffer-software.de>
2018-11-04 14:47:37 +0100

    Swap large image via event listeners on thumbnails

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,22 @@
+
+// Activates the image gallery.
+// Main task: Attach an event listener to each image
+// in the gallery and respond appropriately on click.
+
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    let thumbnails = document
+        .querySelector("#gallery-thumbs")
+        .querySelectorAll("img");
+
+    let largeImage = document
+        .querySelector("#gallery-photo > img");
+
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            // Set clicked image as main image.
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            largeImage.setAttribute("src", newImageSrc);
+        });
+    });
 }
+
